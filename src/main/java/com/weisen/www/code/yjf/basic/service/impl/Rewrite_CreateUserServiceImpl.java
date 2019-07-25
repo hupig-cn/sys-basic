5ff18f08c35ebd0f5844794ee0a4c85da3e6e60d
Administrator <admin@example.com>
2019-07-25 20:01:44 +0800

    修改生成用户

--- src/main/java/com/weisen/www/code/yjf/basic/service/impl/Rewrite_CreateUserServiceImpl.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/impl/Rewrite_CreateUserServiceImpl.java
@@ -1,17 +1,14 @@
 package com.weisen.www.code.yjf.basic.service.impl;
 
-import com.weisen.www.code.yjf.basic.util.TimeUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.weisen.www.code.yjf.basic.domain.Coupon;
 import com.weisen.www.code.yjf.basic.domain.Information;
 import com.weisen.www.code.yjf.basic.domain.Linkaccount;
 import com.weisen.www.code.yjf.basic.domain.Linkuser;
 import com.weisen.www.code.yjf.basic.domain.Userassets;
 import com.weisen.www.code.yjf.basic.domain.Userlinkuser;
 import com.weisen.www.code.yjf.basic.domain.Userlocation;
-import com.weisen.www.code.yjf.basic.repository.CouponRepository;
 import com.weisen.www.code.yjf.basic.repository.InformationRepository;
 import com.weisen.www.code.yjf.basic.repository.LinkaccountRepository;
 import com.weisen.www.code.yjf.basic.repository.LinkuserRepository;
@@ -38,48 +35,16 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 
 	private UserassetsRepository userassetsRepository;
 
-	private CouponRepository couponRepository;
-
 	public Rewrite_CreateUserServiceImpl(LinkuserRepository linkuserRepository,
 			LinkaccountRepository linkaccountRepository, UserlinkuserRepository userlinkuserRepository,
 			UserlocationRepository userlocationRepository, UserassetsRepository userassetsRepository,
-			InformationRepository informationRepository, CouponRepository couponRepository) {
+			InformationRepository informationRepository) {
 		this.linkuserRepository = linkuserRepository;
 		this.linkaccountRepository = linkaccountRepository;
 		this.userlinkuserRepository = userlinkuserRepository;
 		this.userlocationRepository = userlocationRepository;
 		this.userassetsRepository = userassetsRepository;
 		this.informationRepository = informationRepository;
-		this.couponRepository = couponRepository;
-	}
-
-	/**
-	 * 该接口适用于微信或者支付宝扫描授权后创建用户
-	 */
-	@Override
-	public Result createUserByScan(String userId, String token, String accounttype, String recommendId,
-			String coordinate) {
-		// 1.首先在linkuser表中创建用户附加信息
-		Linkuser linkuser = new Linkuser();
-		linkuser.setUserid(userId);
-		linkuser.setCreator(userId);
-		linkuser.setCreatedate("创建时间");
-		linkuserRepository.save(linkuser);
-		// 2.在Linkaccount表中添加用户绑定账号信息
-		Linkaccount linkaccount = new Linkaccount();
-		linkaccount.setUserid(userId);
-		linkaccount.setAccounttype(accounttype);
-		linkaccount.setToken(token);
-		linkaccount.setCreator(userId);
-		linkaccount.setCreatedate("创建时间");
-		linkaccountRepository.save(linkaccount);
-		// 3.在userlinkuser表中创建用户关系信息
-		Userlinkuser userlinkuser = new Userlinkuser();
-		userlinkuser.setUserid(userId);
-		userlinkuser.setRecommendid(recommendId);
-		userlinkuserRepository.save(userlinkuser);
-		createBasicInfo(userId, coordinate);
-		return Result.suc("用户创建成功");
 	}
 
 	/**
@@ -87,7 +52,7 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 	 */
 	@Override
 	public Result createUserByPhone(String userId, String phone) {
-		createBasicInfo(userId, phone);
+		createBasicInfo(userId, phone, "", DateUtils.getDateForNow());
 		return Result.suc("用户创建成功");
 	}
 
@@ -97,18 +62,7 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 	@Override
 	public Result createUserByScanning(String userId, String phone, String referrer) {
 		String thisDate = DateUtils.getDateForNow();
-		Userlinkuser userlinkuser = new Userlinkuser();
-		userlinkuser.setUserid(userId);
-		userlinkuser.setRecommendid(referrer);
-		userlinkuser.setPartner(false);
-		userlinkuser.setProvince(false);
-		userlinkuser.setCity(false);
-		userlinkuser.setCounty(false);
-		userlinkuser.setCreator(userId);
-		userlinkuser.setCreatedate(thisDate);
-		userlinkuser.setModifier(userId);
-		userlinkuser.setModifierdate(thisDate);
-		userlinkuserRepository.save(userlinkuser);// 推荐人
+		createBasicInfo(userId, phone, referrer, thisDate);
 		Information information = new Information();
 		information.setType("推荐消息");
 		information.setSenduserid("1");
@@ -119,50 +73,45 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 		information.setState("未读");
 		information.setWeight("正常");
 		informationRepository.save(information);// 发推荐消息
-		createBasicInfo(userId, phone);
 		return Result.suc("用户创建成功");
 	}
 
-	private void createBasicInfo(String userId, String phone) {
-		Linkuser linkuser = new Linkuser();
-		linkuser.setUserid(userId);
-		linkuser.setPhone(phone);
-        linkuserRepository.save(linkuser);// 用户附加信息
-
-        // 用户关系表
-        Userlinkuser userlinkuser = new Userlinkuser();
-        userlinkuser.setUserid(userId);
-        userlinkuser.setPartner(false);
-        userlinkuser.setProvince(false);
-        userlinkuser.setCity(false);
-        userlinkuser.setCounty(false);
-        userlinkuser.setCreatedate(TimeUtil.getDate());
-        userlinkuserRepository.save(userlinkuser);
-
-		Userlocation userlocation = new Userlocation();
-		userlocation.setUserid(userId);
-		userlocationRepository.save(userlocation);// 用户位置信息
-		Userassets userassets = new Userassets();
-		userassets.setUserid(userId);
-		userassets.setBalance("0");
-		userassets.setUsablebalance("0");
-		userassets.setFrozenbalance("0");
-		userassets.setIntegral("0");
-		userassetsRepository.save(userassets);// 用户资产
-		Coupon Coupon = new Coupon();
-		Coupon.setUserid(userId);
-		Coupon.setSum("0");
-		Coupon.setCoupontype("普通");
-		Coupon.setLineon(true);
-		Coupon.setLineunder(true);
-		Coupon.setIntegral(true);
-		Coupon.setProfit(true);
-		couponRepository.save(Coupon);// 优惠卷
-	}
+	/**
+	 * 该接口适用于扫描商家付款码后创建用户
+	 */
 	@Override
-	public String createUserByScanningMerchant(String userid, String merchantid, String token, String accounttype) {
-		//创建用户推荐信息
+	public String createUserByScanningMerchant(String userid, String token, String accounttype) {
 		String thisDate = DateUtils.getDateForNow();
+		Linkaccount linkaccount = new Linkaccount();
+		linkaccount.setUserid(userid);
+		linkaccount.setAccounttype(accounttype);
+		linkaccount.setToken(token);
+		linkaccount.setCreator(userid);
+		linkaccount.setCreatedate(thisDate);
+		linkaccount.setModifier(userid);
+		linkaccount.setModifierdate(thisDate);
+		linkaccount.setOther(accounttype);
+		linkaccountRepository.save(linkaccount);// 创建账号绑定
+		createBasicInfo(userid, "", "", thisDate);
+		return "创建成功";
+	}
+
+	private void createBasicInfo(String userid, String phone, String merchantid, String thisDate) {
+		Linkuser linkuser = new Linkuser();
+		linkuser.setUserid(userid);
+		linkuser.setPhone(phone);
+		linkuser.setCreator(userid);
+		linkuser.setCreatedate(thisDate);
+		linkuser.setModifier(userid);
+		linkuser.setModifierdate(thisDate);
+		linkuserRepository.save(linkuser);// 用户附加信息
+		Userlocation userlocation = new Userlocation();
+		userlocation.setUserid(userid);
+		userlocation.setCreator(userid);
+		userlocation.setCreatedate(thisDate);
+		userlocation.setModifier(userid);
+		userlocation.setModifierdate(thisDate);
+		userlocationRepository.save(userlocation);// 用户位置信息
 		Userlinkuser userlinkuser = new Userlinkuser();
 		userlinkuser.setUserid(userid);
 		userlinkuser.setRecommendid(merchantid);
@@ -174,8 +123,7 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 		userlinkuser.setCreatedate(thisDate);
 		userlinkuser.setModifier(userid);
 		userlinkuser.setModifierdate(thisDate);
-		userlinkuserRepository.save(userlinkuser);// 推荐人
-		//创建用户资产
+		userlinkuserRepository.save(userlinkuser);// 用户关系表
 		Userassets userassets = new Userassets();
 		userassets.setUserid(userid);
 		userassets.setBalance("0");
@@ -187,17 +135,5 @@ public class Rewrite_CreateUserServiceImpl implements Rewrite_CreateUserService 
 		userassets.setModifier(userid);
 		userassets.setModifierdate(thisDate);
 		userassetsRepository.save(userassets);// 用户资产
-		//创建账号绑定
-		Linkaccount linkaccount = new Linkaccount();
-		linkaccount.setUserid(userid);
-		linkaccount.setAccounttype(accounttype);
-		linkaccount.setToken(token);
-		linkaccount.setCreator(userid);
-		linkaccount.setCreatedate(thisDate);
-		linkaccount.setModifier(userid);
-		linkaccount.setModifierdate(thisDate);
-		linkaccount.setOther(accounttype);
-		linkaccountRepository.save(linkaccount);
-		return "创建成功";
 	}
 }
--- src/main/java/com/weisen/www/code/yjf/basic/web/rest/Rewrite_CreateUserResource.java
+++ src/main/java/com/weisen/www/code/yjf/basic/web/rest/Rewrite_CreateUserResource.java
@@ -1,37 +1,18 @@
 package com.weisen.www.code.yjf.basic.web.rest;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
-
 import javax.validation.Valid;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.weisen.www.code.yjf.basic.service.Rewrite_CreateUserService;
 import com.weisen.www.code.yjf.basic.service.dto.LinkuserDTO;
-import com.weisen.www.code.yjf.basic.service.dto.UserlinkuserDTO;
 import com.weisen.www.code.yjf.basic.util.Result;
-import com.weisen.www.code.yjf.basic.web.rest.errors.BadRequestAlertException;
 
-import io.github.jhipster.web.util.HeaderUtil;
-import io.github.jhipster.web.util.PaginationUtil;
-import io.github.jhipster.web.util.ResponseUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -44,18 +25,6 @@ public class Rewrite_CreateUserResource {
 
 	public Rewrite_CreateUserResource(Rewrite_CreateUserService createUserService) {
 		this.createUserService = createUserService;
-	}
-
-	/**
-	 * 支付宝或者微信扫描商家收款二维码创建用户接口
-	 * 
-	 * @return
-	 */
-	@PostMapping("/public/user/createUserByScan")
-	@ApiOperation(value = "支付宝或者微信扫描商家收款二维码创建用户接口")
-	public Result createUserByScan(String userId, String token, String accounttype, String recommendId,
-			String coordinate) {
-		return createUserService.createUserByScan(userId, token, accounttype, recommendId, coordinate);
 	}
 
 	/**
@@ -82,14 +51,13 @@ public class Rewrite_CreateUserResource {
 	}
 
 	/**
-	 * 手机扫描商家付款码被绑定到商户下，创建用户
+	 * 手机扫描商家付款码，创建用户
 	 * 
 	 * @return
 	 */
 	@GetMapping("/public/user/createUserByScanningMerchant")
-	@ApiOperation(value = "手机扫描商家付款码被绑定到商户下，创建用户")
-	public String createUserByScanningMerchant(@RequestParam String userid, String merchantid, String token,
-			String accounttype) {
-		return createUserService.createUserByScanningMerchant(userid, merchantid, token, accounttype);
+	@ApiOperation(value = "手机扫描商家付款码，创建用户")
+	public String createUserByScanningMerchant(@RequestParam String userid, String token, String accounttype) {
+		return createUserService.createUserByScanningMerchant(userid, token, accounttype);
 	}
 }
