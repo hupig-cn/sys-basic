ccd330e3812f3f4378c468fef1f306df644a6489
Administrator <admin@example.com>
2019-07-26 14:57:16 +0800

    修复一个实名的弹窗

--- src/main/java/com/weisen/www/code/yjf/basic/service/impl/Rewrite_LinkuserServiceImpl.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/impl/Rewrite_LinkuserServiceImpl.java
@@ -56,7 +56,7 @@ public class Rewrite_LinkuserServiceImpl implements Rewrite_LinkuserService {
 	public String queryRealName(String userid) {
 		Linkuser linkuser = rewrite_LinkuserRepository.findByUserid(userid);
 		if (null != linkuser) {
-			if (linkuser.getIdcard().length() == 18 && linkuser.getName() != null) {
+			if (null != linkuser.getIdcard() && "".equals(linkuser.getIdcard()) && linkuser.getIdcard().length() == 18 && null != linkuser.getName()) {
 				return "已认证";
 			}
 		}
