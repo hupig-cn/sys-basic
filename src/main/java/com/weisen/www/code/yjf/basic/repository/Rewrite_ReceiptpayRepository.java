9e323fb7050cbf3ca6711b7dea22d7ca80b7fb1c
jianglinyi <605386265@qq.com>
2019-08-02 09:44:55 +0800

    m

--- src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_ReceiptpayRepository.java
+++ src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_ReceiptpayRepository.java
@@ -23,8 +23,8 @@ public interface Rewrite_ReceiptpayRepository extends JpaRepository<Receiptpay, 
 //	List<Receiptpay> getReceiptpayByUseridAndTime(String userId, String startTime, String endTime, String dealtype);
 
     // 根据用户id，时间范围,交易类型 查询明细
-    @Query(value = "select id,dealtype,userid,sourcer,benefit,amount,bonus,happendate,freezedate,dealstate,"
-        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other,payway "
+    @Query(value = "select id,dealtype,userid,sourcer,benefit,payway,amount,bonus,happendate,freezedate,dealstate,"
+        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other "
         + "from receiptpay where userid=?1 and createdate > str_to_date(?2,'%Y-%m-%d %H:%i:%s') "
         + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') and dealtype = ?4", nativeQuery = true)
     List<Receiptpay> getReceiptpayByUseridAndTime(String userId, String startTime, String endTime, String dealtype);
@@ -33,19 +33,19 @@ public interface Rewrite_ReceiptpayRepository extends JpaRepository<Receiptpay, 
 	List<Receiptpay> findAllByUseridAndDealtype(String userId,String dealtype);
 
 	// 根据类型，查询时间段内的收益数据
-    @Query(value = "select id,dealtype,userid,sourcer,benefit,amount,bonus,happendate,freezedate,dealstate,"
-        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other,payway "
+    @Query(value = "select id,dealtype,userid,sourcer,benefit,payway,amount,bonus,happendate,freezedate,dealstate,"
+        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other "
         + "from receiptpay where userid=?1 and createdate > str_to_date(?2,'%Y-%m-%d %H:%i:%s') "
         + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') and dealtype in (?4,?5)", nativeQuery = true)
     List<Receiptpay> findInfoByTime(String userId, String startTime, String endTime, String four,String five);
 
-    @Query(value = "select id,dealtype,userid,sourcer,benefit,amount,bonus,happendate,freezedate,dealstate,"
-        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other,payway "
+    @Query(value = "select id,dealtype,userid,sourcer,benefit,payway,amount,bonus,happendate,freezedate,dealstate,"
+        + "creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other "
         + "from receiptpay where userid=?1 and dealtype in (?2,?3)", nativeQuery = true)
     List<Receiptpay> getAllInfo(String userid,String two,String three);
 
     //倒叙分页查询商家流水
-    @Query(value = "select id,dealtype,userid,sourcer,benefit,amount,bonus,happendate,freezedate,dealstate,creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other,payway from receiptpay where userid = ?1 and dealtype=?2 order by createdate desc limit ?3,?4",nativeQuery = true)
+    @Query(value = "select id,dealtype,userid,sourcer,benefit,payway,amount,bonus,happendate,freezedate,dealstate,creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other from receiptpay where userid = ?1 and dealtype=?2 order by createdate desc limit ?3,?4",nativeQuery = true)
     List<Receiptpay> getAllByMerchantAndType(String userid,String type,int indexPage,int pageSize);
 
 }
