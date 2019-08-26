da76959af3e5637941a5be59d0e851597fa97cc0
伍家成 <569475308@qq.com>
2019-08-26 09:25:56 +0800

    充值余额和扣减余额

---
+++ src/main/java/com/weisen/www/code/yjf/basic/service/dto/submit_dto/Rewrite_000_RechangeDTO.java
@@ -0,0 +1,24 @@
+package com.weisen.www.code.yjf.basic.service.dto.submit_dto;
+
+public class Rewrite_000_RechangeDTO {
+
+	private String account;
+	
+	private String yue;
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getYue() {
+		return yue;
+	}
+
+	public void setYue(String yue) {
+		this.yue = yue;
+	}
+}
