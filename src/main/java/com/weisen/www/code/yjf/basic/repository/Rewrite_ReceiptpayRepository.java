7beaa115af5ca9bc569bd0216e8114435fa4bb35
shenxx <1428618324@qq.com>
2019-10-23 17:35:36 +0800

    傻逼欣

--- src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_ReceiptpayRepository.java
+++ src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_ReceiptpayRepository.java
@@ -59,12 +59,12 @@ public interface Rewrite_ReceiptpayRepository extends JpaRepository<Receiptpay, 
  // 根据类型，查询时间段内的收益数据
     @Query(value = "select * "
         + "from receiptpay where userid=?1 and createdate > str_to_date(?2,'%Y-%m-%d %H:%i:%s') "
-        + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') ", nativeQuery = true)
+        + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') and dealtype != 6  ", nativeQuery = true)
     List<Receiptpay> findByTimeAndUserid(String userId, String startTime, String endTime);
 
     @Query(value = "select * "
         + "from receiptpay where sourcer=?1 and createdate > str_to_date(?2,'%Y-%m-%d %H:%i:%s') "
-        + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') ", nativeQuery = true)
+        + "and createdate < str_to_date(?3,'%Y-%m-%d %H:%i:%s') and dealtype != 6 ", nativeQuery = true)
     List<Receiptpay> findByTimeAndSourcer(String sourcer, String startTime, String endTime);
 
     Receiptpay findReceiptpayById(Long id);
--- src/main/java/com/weisen/www/code/yjf/basic/service/rewrite/impl/Rewrite_IncomeDetailsServiceImpl.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/rewrite/impl/Rewrite_IncomeDetailsServiceImpl.java
@@ -12,6 +12,7 @@ import com.weisen.www.code.yjf.basic.repository.Rewrite_LinkuserRepository;
 import com.weisen.www.code.yjf.basic.repository.Rewrite_ReceiptpayRepository;
 import com.weisen.www.code.yjf.basic.repository.rewrite.Rewrite_IncomeDetailsRepository;
 import com.weisen.www.code.yjf.basic.service.rewrite.Rewrite_IncomeDetailsService;
+import com.weisen.www.code.yjf.basic.service.rewrite.dto.Rewrite_GetIncomeListDTO;
 import com.weisen.www.code.yjf.basic.util.Result;
 
 @Service
@@ -41,15 +42,19 @@ public class Rewrite_IncomeDetailsServiceImpl implements Rewrite_IncomeDetailsSe
     //获取推荐人列表
 	@Override
 	public Result getRecommendList(String recommendId) {
+		Long recommendIdCount = incomeDetailsRepository.findByRecommendIdCount(recommendId);
 		List<Userlinkuser> recommends = incomeDetailsRepository.findByUserid(recommendId);
+		Rewrite_GetIncomeListDTO getIncomeListDTO = new Rewrite_GetIncomeListDTO();
 		for (Userlinkuser recommend : recommends) {
 			String userid = recommend.getUserid();
 			Linkuser linkuser = linkuserRepository.findByUserid(userid);
 			String phone = linkuser.getPhone();
+			String createdate = linkuser.getCreatedate();
 			Receiptpay receiptpayUid = receiptpayRepository.findReceiptpayByUid(userid);
 			
-//			Receiptpay findReceiptpayByUid = receiptpayRepository.findReceiptpayByUid(userlinkuser.getRecommendid());
-//			findReceiptpayByUid.getDealtype();
+			getIncomeListDTO.setNickName("好友："+phone.substring(0, 2)+"****"+phone.substring(7, 10));
+			getIncomeListDTO.setCreatedate(createdate);
+			getIncomeListDTO.setRecommendIdCount(recommendIdCount);
 			
 			
 		}
