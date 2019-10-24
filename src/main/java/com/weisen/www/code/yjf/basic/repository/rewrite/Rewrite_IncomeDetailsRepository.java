318b3e507525d87b011e51df857517e19a7d214e
shenxx <1428618324@qq.com>
2019-10-24 09:40:33 +0800

    连接login数据库中的jhi_user表

--- src/main/java/com/weisen/www/code/yjf/basic/repository/rewrite/Rewrite_IncomeDetailsRepository.java
+++ src/main/java/com/weisen/www/code/yjf/basic/repository/rewrite/Rewrite_IncomeDetailsRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.weisen.www.code.yjf.basic.domain.User;
 import com.weisen.www.code.yjf.basic.domain.Userlinkuser;
 
 public interface Rewrite_IncomeDetailsRepository  extends JpaRepository<Userlinkuser, Long> {
@@ -13,6 +14,11 @@ public interface Rewrite_IncomeDetailsRepository  extends JpaRepository<Userlink
 	@Query(value = "select count(userid) from userlinkuser where recommendid = ?1", nativeQuery = true)
 	Long findByRecommendIdCount(String recommendid);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 	
-	//根据uid找到该条信息
-	List<Userlinkuser> findByUserid(String userId);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+	//根据用户uid找到信息
+	List<Userlinkuser> findByRecommendId(String userId);
+	
+	//根据用户id和时间段找到信息
+	@Query(value = "select userid,createdate from userlinkuser where recommendid = ?1 between firstTime = ?2 and lastTime = ?1", nativeQuery = true)
+	List<Userlinkuser> findByRecommendIdAndTime(String recommendid,String firstTime,String lastTime);
+	
 }
