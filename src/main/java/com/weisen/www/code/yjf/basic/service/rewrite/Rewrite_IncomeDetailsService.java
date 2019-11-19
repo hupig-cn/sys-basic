7bba22a9af95699cbfa131470c885023f151cf1a
jinshui <1511328878@qq.com>
2019-11-19 16:31:28 +0800

    查询推荐榜前十位用户

--- src/main/java/com/weisen/www/code/yjf/basic/service/rewrite/Rewrite_IncomeDetailsService.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/rewrite/Rewrite_IncomeDetailsService.java
@@ -1,27 +1,27 @@
 package com.weisen.www.code.yjf.basic.service.rewrite;
 
-
 import com.weisen.www.code.yjf.basic.service.rewrite.dto.Rewrite_GetIncomeAfferentDTO;
 import com.weisen.www.code.yjf.basic.util.Result;
 
 public interface Rewrite_IncomeDetailsService {
-    //获取各推荐人总数
-	 Result getRecommendTotal(String recommendId);
-	 
-	 
-	 //获取各推荐人列表
-	 Result getRecommendList(Rewrite_GetIncomeAfferentDTO getIncomeAfferentDTO);
-	 
-	 //获取各推荐人列表
-	 Result getProfitList(String userId);
-	 
-	 //用户信息列表
-	 Result userInformationList(String userId,Integer Type,Integer pageNum ,Integer pageSize);
-	 
-	 //查询用户商家端收益列表倒叙(重写)
-	 Result newFindMerchantProfitInfo(String userid, Integer startPage, Integer pageSize);
-	 
-	 
-	 //获取各推荐人列表(为兼容新版本写的)
-	 Result getProfitList2(String userId);
+	// 获取各推荐人总数
+	Result getRecommendTotal(String recommendId);
+
+	// 获取各推荐人列表
+	Result getRecommendList(Rewrite_GetIncomeAfferentDTO getIncomeAfferentDTO);
+
+	// 获取各推荐人列表
+	Result getProfitList(String userId);
+
+	// 用户信息列表
+	Result userInformationList(String userId, Integer Type, Integer pageNum, Integer pageSize);
+
+	// 查询用户商家端收益列表倒叙(重写)
+	Result newFindMerchantProfitInfo(String userid, Integer startPage, Integer pageSize);
+
+	// 获取各推荐人列表(为兼容新版本写的)
+	Result getProfitList2(String userId);
+
+	// 查询推荐榜前十位用户
+	Result recommendationList(String userId);
 }
--- src/main/java/com/weisen/www/code/yjf/basic/web/rewrite/rewrite_001_收益明细/Rewrite_IncomeDetailsResource.java
+++ src/main/java/com/weisen/www/code/yjf/basic/web/rewrite/rewrite_001_收益明细/Rewrite_IncomeDetailsResource.java
@@ -1,6 +1,5 @@
 package com.weisen.www.code.yjf.basic.web.rewrite.rewrite_001_收益明细;
 
-
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -21,7 +20,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 /**
- * REST controller for managing {@link com.weisen.www.code.yjf.basic.domain.Withdrawal}.
+ * REST controller for managing
+ * {@link com.weisen.www.code.yjf.basic.domain.Withdrawal}.
  */
 @RestController
 @RequestMapping("/api/incomeDetails")
@@ -30,96 +30,110 @@ public class Rewrite_IncomeDetailsResource {
 
 	private final Logger log = LoggerFactory.getLogger(Rewrite_IncomeDetailsResource.class);
 
-    private final Rewrite_IncomeDetailsService incomeDetailsService;
+	private final Rewrite_IncomeDetailsService incomeDetailsService;
 
-    public Rewrite_IncomeDetailsResource(Rewrite_IncomeDetailsService incomeDetailsService) {
-        this.incomeDetailsService = incomeDetailsService;
-    }
-    /**
-     * 获取各推荐人总数
-     *
-    	 * @author sxx
-    	 * @date 2019-10-23 15:55:31
-     */
-    @PostMapping("/getRecommend/Total")
-    @ApiOperation("获取推荐人数量信息")
-    public ResponseEntity<?> getRecommendTotal(@RequestParam String recommendId){
+	public Rewrite_IncomeDetailsResource(Rewrite_IncomeDetailsService incomeDetailsService) {
+		this.incomeDetailsService = incomeDetailsService;
+	}
+
+	/**
+	 * 获取各推荐人总数
+	 *
+	 * @author sxx
+	 * @date 2019-10-23 15:55:31
+	 */
+	@PostMapping("/getRecommend/Total")
+	@ApiOperation("获取推荐人数量信息")
+	public ResponseEntity<?> getRecommendTotal(@RequestParam String recommendId) {
 		Result result = incomeDetailsService.getRecommendTotal(recommendId);
-		log.debug("访问地址: {},传入值: {},返回值: {}", "/getRecommend/Total",recommendId , result);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/getRecommend/Total", recommendId, result);
 		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
+	}
 
-    /**
-     * 获取推荐流水明细
-     *
-    	 * @author sxx
-    	 * @date 2019-10-23 15:55:31
-     */
-    @PostMapping("/getRecommend/List")
-    @ApiOperation("获取推荐流水明细")
-    public ResponseEntity<?> getRecommendList(@RequestBody Rewrite_GetIncomeAfferentDTO getIncomeAfferentDTO){
-    	Result result = incomeDetailsService.getRecommendList(getIncomeAfferentDTO);
-    	log.debug("访问地址: {},传入值: {},返回值: {}", "/getRecommend/Total",getIncomeAfferentDTO , result);
-    	return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
+	/**
+	 * 获取推荐流水明细
+	 *
+	 * @author sxx
+	 * @date 2019-10-23 15:55:31
+	 */
+	@PostMapping("/getRecommend/List")
+	@ApiOperation("获取推荐流水明细")
+	public ResponseEntity<?> getRecommendList(@RequestBody Rewrite_GetIncomeAfferentDTO getIncomeAfferentDTO) {
+		Result result = incomeDetailsService.getRecommendList(getIncomeAfferentDTO);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/getRecommend/Total", getIncomeAfferentDTO, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
 
-    /**
-     * 收益列表
-     *
-    	 * @author sxx
-    	 * @date 2019-10-26 15:55:31
-     */
-    @PostMapping("/Profit/List")
-    @ApiOperation("收益列表")
-    public ResponseEntity<?> getProfitList(@RequestParam String userId){
-        Result result = incomeDetailsService.getProfitList(userId);
-        log.debug("访问地址: {},传入值: {},返回值: {}", "/Profit/List",userId, result);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
-    
-    /**
-     * 收益列表
-     *
-    	 * @author sxx
-    	 * @date 2019-10-26 15:55:31
-     */
-    @PostMapping("/Profit/List2")
-    @ApiOperation("收益列表")
-    public ResponseEntity<?> getProfitList2(@RequestParam String userId){
-        Result result = incomeDetailsService.getProfitList2(userId);
-        log.debug("访问地址: {},传入值: {},返回值: {}", "/NewProfit/List",userId, result);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
+	/**
+	 * 收益列表
+	 *
+	 * @author sxx
+	 * @date 2019-10-26 15:55:31
+	 */
+	@PostMapping("/Profit/List")
+	@ApiOperation("收益列表")
+	public ResponseEntity<?> getProfitList(@RequestParam String userId) {
+		Result result = incomeDetailsService.getProfitList(userId);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/Profit/List", userId, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
 
-    /**
-     * 推荐用户信息列表
-     *
-    	 * @author sxx
-    	 * @date 2019-10-31 15:55:31
-     */
-    @PostMapping("/UserInformation/List")
-    @ApiOperation("推荐用户信息列表")
-    public ResponseEntity<?> userInformationList(@RequestParam String userId,@RequestParam Integer Type
-    		,@RequestParam Integer pageNum,@RequestParam Integer pageSize){
-        Result result = incomeDetailsService.userInformationList(userId,Type,pageNum,pageSize);
-        log.debug("访问地址: {},传入值: {},返回值: {}", "/UserInformation/List",userId, result);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
-    
-    /**
-     * 查询用户商家端收益列表倒叙(重写)
-     *
-    	 * @author sxx
-    	 * @date 2019-11-01 15:55:31
-     */
-    @PostMapping("/newFindMerchantProfitInfo")
-    @ApiOperation("查询用户商家端收益列表倒叙(重写)")
-    public ResponseEntity<Result> newFindMerchantProfitInfo(@RequestParam String userid, 
-    		@RequestParam Integer startPage, @RequestParam Integer pageSize) {
-        Result result = incomeDetailsService.newFindMerchantProfitInfo(userid,startPage,pageSize);
-        log.debug("访问地址: {},传入值: {},返回值: {}", "/newFindMerchantProfitInfo",userid+startPage+pageSize, result);
-        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
-    }
+	/**
+	 * 收益列表
+	 *
+	 * @author sxx
+	 * @date 2019-10-26 15:55:31
+	 */
+	@PostMapping("/Profit/List2")
+	@ApiOperation("收益列表")
+	public ResponseEntity<?> getProfitList2(@RequestParam String userId) {
+		Result result = incomeDetailsService.getProfitList2(userId);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/NewProfit/List", userId, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
 
+	/**
+	 * 推荐用户信息列表
+	 *
+	 * @author sxx
+	 * @date 2019-10-31 15:55:31
+	 */
+	@PostMapping("/UserInformation/List")
+	@ApiOperation("推荐用户信息列表")
+	public ResponseEntity<?> userInformationList(@RequestParam String userId, @RequestParam Integer Type,
+			@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+		Result result = incomeDetailsService.userInformationList(userId, Type, pageNum, pageSize);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/UserInformation/List", userId, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
+
+	/**
+	 * 查询用户商家端收益列表倒叙(重写)
+	 *
+	 * @author sxx
+	 * @date 2019-11-01 15:55:31
+	 */
+	@PostMapping("/newFindMerchantProfitInfo")
+	@ApiOperation("查询用户商家端收益列表倒叙(重写)")
+	public ResponseEntity<Result> newFindMerchantProfitInfo(@RequestParam String userid,
+			@RequestParam Integer startPage, @RequestParam Integer pageSize) {
+		Result result = incomeDetailsService.newFindMerchantProfitInfo(userid, startPage, pageSize);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/newFindMerchantProfitInfo", userid + startPage + pageSize, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
+
+	/**
+	 * 查询推荐榜前十位
+	 *
+	 * @author LuoJinShui
+	 * @date 2019-11-19 10:43:31
+	 */
+	@PostMapping("/Recommendation/List")
+	@ApiOperation("查询推荐榜前十位用户")
+	public ResponseEntity<Result> recommendationList(@RequestParam String userId) {
+		Result result = incomeDetailsService.recommendationList(userId);
+		log.debug("访问地址: {},传入值: {},返回值: {}", "/Recommendation/List", userId, result);
+		return ResponseUtil.wrapOrNotFound(Optional.ofNullable(result));
+	}
 
 }
