90333ffc5e9172642c972ec986654beedda895ea
shenxx <1428618324@qq.com>
2019-11-26 10:05:36 +0800

    新增    自动更新图片宽高数据   接口

--- src/main/java/com/weisen/www/code/yjf/basic/repository/FilesRepository.java
+++ src/main/java/com/weisen/www/code/yjf/basic/repository/FilesRepository.java
@@ -18,5 +18,9 @@ public interface FilesRepository extends JpaRepository<Files, Long> {
 	//通过id找到该记录
 	@Query(value = "select * from files where id = ?1", nativeQuery = true)
 	Files findByIds(Long id);
+	
+	//通过id找到该记录
+	@Query(value = "select * from files where height is null and width is null", nativeQuery = true)
+	List<Files> findByHeightAndWidthNotNull();
 
 }
