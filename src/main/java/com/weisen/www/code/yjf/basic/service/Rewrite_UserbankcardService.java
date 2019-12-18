9e01a0a277202a332442ef01e4d3e2ce96f878d4
jinshui <1511328878@qq.com>
2019-12-18 11:03:27 +0800

    重写用户添加银行卡

--- src/main/java/com/weisen/www/code/yjf/basic/service/Rewrite_UserbankcardService.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/Rewrite_UserbankcardService.java
@@ -3,6 +3,7 @@ package com.weisen.www.code.yjf.basic.service;
 import java.util.List;
 
 import com.weisen.www.code.yjf.basic.service.dto.UserbankcardDTO;
+import com.weisen.www.code.yjf.basic.service.dto.submit_dto.Rewrite_UserCardDTO;
 import com.weisen.www.code.yjf.basic.util.Result;
 
 public interface Rewrite_UserbankcardService {
@@ -18,9 +19,12 @@ public interface Rewrite_UserbankcardService {
 
 	// 用户删除银行卡
 	Result deleteBackCard(Long bankcardId);
-	
+
 	// 查询银行卡列表
 	Result findAllBankCard(String userId);
-	
+
 	Result deleteBackCards(List<String> bankcardId);
+
+	// 重写用户添加银行卡
+	Result createBankCard2(Rewrite_UserCardDTO rewrite_UserCardDTO);
 }
