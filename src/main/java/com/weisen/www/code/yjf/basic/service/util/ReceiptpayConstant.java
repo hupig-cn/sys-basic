e1482887ea066478508fd983c4869c5ba3207b0e
伍家成 <569475308@qq.com>
2020-01-02 16:52:24 +0800

    充值

--- src/main/java/com/weisen/www/code/yjf/basic/service/util/ReceiptpayConstant.java
+++ src/main/java/com/weisen/www/code/yjf/basic/service/util/ReceiptpayConstant.java
@@ -6,9 +6,9 @@ public class ReceiptpayConstant {
     
     public static final String WECHAT_PAY = "2"; // 微信支出
 
-    public static final String BALANCE_INCOME = "3"; // 余额收入
+    public static final String BALANCE_INCOME = "3"; // 余额收入【应该都是商家收款吧】
 
-    public static final String BALANCE_PAY = "4"; // 余额支出
+    public static final String BALANCE_PAY = "4"; // 余额支出 【APP扫码付款，购买商品】
 
     public static final String INTEGRAL_PAY = "5"; // 积分支出
 
@@ -23,7 +23,15 @@ public class ReceiptpayConstant {
     public static final String BALANCE_INCOME_PER = "10"; // 合伙人分销收入
     
     public static final String WITHDRAWAL_PAY = "11"; // 提现成功(支出)
+    
+    public static final String RECHARGE_BALANCE = "12"; // 后台充值余额(收入)
 
+    public static final String REDUCT_BALANCE = "13"; // 后台扣减余额(支出)
+    
+    public static final String RECHARGE_INTEGRAL = "14"; // 后台充值余额(收入)
+
+    public static final String REDUCT_INTEGRAL = "15"; // 后台扣减余额(支出)
+    
     public static final String INCOME = "2"; // 收入
 
     public static final String PAY = "1"; // 支出
