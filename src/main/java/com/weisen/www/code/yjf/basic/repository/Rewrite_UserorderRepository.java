b31c98a54e6bb7114d37a6efc849ad66808ee3c1
伍家成 <569475308@qq.com>
2020-01-06 17:12:24 +0800

    666

--- src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_UserorderRepository.java
+++ src/main/java/com/weisen/www/code/yjf/basic/repository/Rewrite_UserorderRepository.java
@@ -1,6 +1,7 @@
 package com.weisen.www.code.yjf.basic.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,11 +11,19 @@ import com.weisen.www.code.yjf.basic.domain.Userorder;
 
 @Repository
 public interface Rewrite_UserorderRepository extends JpaRepository<Userorder, Long> {
-
+	
 	// 分页查询订单列表
     @Query(value = "select id,ordercode,orderstatus,sum,userid,payee,payway,payresult,paytime,concession,rebate,creator,createdate,modifier,modifierdate,modifiernum,logicdelete,other,express_company,express_no "
     		+ "from userorder where (?1 is null or userid = ?1) and (?2 is null or ordercode = ?2) and (?3 is null or orderstatus = ?3) and other > 1 order by createdate DESC limit ?4,?5",nativeQuery = true)
     List<Userorder> getOrderList(String userid,String ordercode,String orderstatus,int pageNum,int pageSize);
+    
+
+	@Query(value = "select a.ordercode,a.userid,c.model,c.specifications,c.price,b.num,a.sum,b.consignee,b.mobile,b.address,a.orderstatus,a.payway,a.paytime,a.express_no,a.express_company "
+			+ "from userorder a left join shopmall.jhi_order b on b.bigorder = a.id "
+			+ "left join shopmall.specifications c on c.id = a.other "
+			+ "where (?1 is null or userid = ?1) and (?2 is null or ordercode = ?2) "
+			+ "and (?3 is null or orderstatus = ?3) and other > 1 order by createdate DESC limit ?4,?5",nativeQuery = true)
+	List<Map<String,Object>> getOrderList2(String userid,String ordercode,String orderstatus,int pageNum,int pageSize);
 
 	// 分页查询订单列表
     @Query(value = "select count(*) from userorder where (?1 is null or userid = ?1) and (?2 is null or ordercode = ?2) and (?3 is null or orderstatus = ?3) and other > 1",nativeQuery = true)
